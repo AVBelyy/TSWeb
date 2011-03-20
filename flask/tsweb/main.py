e0db85916b45d88b909120da20595754dabde670
Maxim <kolmax94@gmail.com>
2011-03-21 00:58:05 +0300

    Restruction of URLs and page processing

--- flask/tsweb/main.py
+++ flask/tsweb/main.py
@@ -6,35 +6,62 @@ from flask import Flask, render_template, request, session, redirect, url_for
 tswebapp = Flask('tsweb')
 tswebapp.secret_key = '123asd'
 
-@tswebapp.route('/', methods=['GET', 'POST'])
-@tswebapp.route('/index', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        if request.args.get('op', '') == 'logout':
-            tm = ', {0}'.format(session['team']) if session.get('cookie_state', 0) > 0 else ''
-            session.pop('cookie_state', None)
-            return render_template("redirect.html", text="Thanks for logging out{0}!".format(tm))
-        elif session.get('cookie_state', 0) <= 0:
-            return render_template("index.html",
-                    error="Your session data is missing, expired or invalid." if session.get('cookie_state', 0) < 0 else '')
+def redirector(url, **kwargs):
+    res = redirect(url)
+    res.response = [render_template("redirect.html", **kwargs)]
 
+    return res
+
+@tswebapp.route('/')
+@tswebapp.route('/index')
+def index():
+    if 'team' in session:
         return format_main_page()
     else:
-        if request.form['op'] == 'changecontest':
-            session['contestid'] = request.form['newcontestid']
-            session['cookie_state'] = 2
-            return render_template("redirect.html", text="Your contest has been changed to {0}, {1}".format(session['contestid'], request.form['team']))
-        elif request.form['op'] == 'login':
-            if not testsys.valid_teamname(request.form['team']):
-                return render_template("error.html", text="Invalid team name")
-            if not request.form['password']:
-                return render_template("error.html", text="Non-empty password expected")
-            session['cookie_state'] = 2
-            session['team'] = request.form['team']
-            session['password'] = request.form['password']
-            return render_template("redirect.html", text="Thank you for logging in, {0}!".format(request.form['team']))
-        elif session['cookie_state'] <= 0:
-            return render_template("index.html", error="Your session data is missing, expired or invalid.")
+        return render_template("index.html")
+#    else:
+#        if request.form['op'] == 'changecontest':
+#            session['contestid'] = request.form['newcontestid']
+#            session['cookie_state'] = 2
+#            return render_template("redirect.html", text="Your contest has been changed to {0}, {1}".format(session['contestid'], request.form['team']))
+
+@tswebapp.route('/logout')
+def logout():
+    tm = ', {0}'.format(session['team']) if 'team' in session else ''
+    session.pop('team', None)
+    return redirector(url_for('index'), text="Thanks for logging out{0}!".format(tm))
+
+@tswebapp.route('/login', methods=['POST'])
+def login():
+    if not testsys.valid_teamname(request.form['team']):
+        return render_template("error.html", text="Invalid team name")
+
+    if not request.form['password']:
+        return render_template("error.html", text="Non-empty password expected")
+
+    MSG = testsys.Channel('MSG')
+    try:
+        MSG.open(1)
+    except testsys.ConnectionFailedException:
+        return render_template("error.html", text="Cannot connect to TESTSYS")
+
+    MSG.send({
+        'Team': request.form['team'],
+        'Password': request.form['password'],
+        'ContestId': request.form.get('contestid', ''),
+        'AllMessages': 'Yes',
+        'DisableUnrequested': 1})
+
+    ans = MSG.recv()
+    if 'Error' in ans:
+        return render_template("error.html", text="Error logging in: {0}".format(ans['Error']))
+
+    session['team'] = request.form['team']
+    session['password'] = request.form['password']
+    session['contestid'] = ans.get('ContestId', '')
+    session['team_name'] = ans.get('TeamName', '').decode('cp866')
+
+    return redirector(url_for('index'), text="Thank you for logging in, {0}!".format(session['team']))
 
 def format_main_page():
     MSG = testsys.Channel('MSG')
